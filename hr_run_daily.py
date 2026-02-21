06a67e9aa5fc0b8dc0e168102c8f2f08f2f0875a
reddragon047 <nicolas.dina@yahoo.com>
2026-02-21 07:31:38 -0600

    Add import market_clv

--- hr_run_daily.py
+++ hr_run_daily.py
@@ -14,6 +14,7 @@ import statsapi
 from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.isotonic import IsotonicRegression
+from inputs import market_clv
 
 
 # -------------------------
